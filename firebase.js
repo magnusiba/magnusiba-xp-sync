583203825406bb2540132636884699ff4ce30a76
magnusiba <magnus.sam.bd@gmail.com>
2025-04-04 04:11:35 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,6 +4,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    // 🔥 This handles line breaks properly without any escaping issues
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
 });
